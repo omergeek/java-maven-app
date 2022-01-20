d8cbe54557dd63a4964144f180f5bc16aac85b1b
omergeek <97628194+omergeek@users.noreply.github.com>
2022-01-20 15:26:12 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,3 +1,17 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t omer2020/my-repo:2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push omer2020/my-repo:2.0'
+    }
+} 
+
 def deployApp() {
     echo 'deploying the application...'
 } 
